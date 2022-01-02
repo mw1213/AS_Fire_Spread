39c80af3e2da5c5addbcde3e2ecbd61ce4209b94
mw1213 <mw1213@wp.pl>
2022-01-02 13:03:22 +0100

    add wind influence

--- src/symulation.py
+++ src/symulation.py
@@ -3,24 +3,42 @@ import matplotlib.pyplot as plt
 from matplotlib import animation
 from matplotlib import colors
 
-# Create a forest fire animation based on a simple cellular automaton model.
-# The maths behind this code is described in the scipython blog article
-# at https://scipython.com/blog/the-forest-fire-model/
-# Christian Hill, January 2016.
-# Updated January 2020.
+
+# based on model from article
+# https://scipython.com/blog/the-forest-fire-model/
 
 # Displacements from a cell to its eight nearest neighbours
 neighbourhood = ((-1,-1), (-1,0), (-1,1), (0,-1), (0, 1), (1,-1), (1,0), (1,1))
+
+
+# Wind influence based on its direction: from top left clockwise
+wind_max, wind_most, wind_ok, wind_bad, wind_no = 2, 1.25, 0.75, 0.25, 0
+wind_infulence =[
+    [wind_max, wind_most, wind_ok, wind_most, wind_bad, wind_ok, wind_bad, wind_no],
+    [wind_most, wind_max, wind_most, wind_ok, wind_ok, wind_bad, wind_no, wind_bad],
+    [wind_ok, wind_most, wind_max, wind_bad, wind_most, wind_no, wind_bad, wind_ok],
+    [wind_bad, wind_ok, wind_most, wind_no, wind_max, wind_bad, wind_ok, wind_most],
+    [wind_no, wind_bad, wind_ok, wind_bad, wind_most, wind_ok, wind_most, wind_max],
+    [wind_bad, wind_no, wind_bad, wind_ok, wind_ok, wind_most, wind_max, wind_most],
+    [wind_ok, wind_bad, wind_no, wind_most, wind_bad, wind_max, wind_most, wind_ok],
+    [wind_most, wind_ok, wind_bad, wind_max, wind_no, wind_most, wind_ok, wind_bad],
+    [1, 1, 1, 1, 1, 1, 1, 1]
+]
+
+# Wind direction setting
+se, e, ne, n, nw, w, sw, s, no_wind_source = 0,1,2,3,4,5,6,7,8
+wind_direction = e
+
+# cell types
 EMPTY, DEAD_TREE, OLD_TREE, TREE, SAPLING, STONE, WATER, SPARKS, FIRE, COALS = 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
-# Colours for visualization: brown for EMPTY, dark green for TREE and orange
-# for FIRE. Note that for the colormap to work, this list and the bounds list
-# must be one larger than the number of different values in the array.
 TREE_TYPES = DEAD_TREE, OLD_TREE, TREE, SAPLING
+
+# maping cell types to colors
 colors_list = ['black','sienna', 'darkolivegreen', 'darkgreen', 'lime', 'gray', 'blue', 'coral', 'firebrick', 'orange']
 cmap = colors.ListedColormap(colors_list)
 bounds = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-# scale 0-1: 1 means no reduction
+# water influence in scale 0-1: 1 means no reduction
 water_reduction_rate = 1# 0.0000001
 
 norm = colors.BoundaryNorm(bounds, cmap.N)
@@ -43,26 +61,34 @@ def calculate_fire_at_position(X, X1, ix, iy, tree_type, near_water):
         case 4:
             likelyhood_of_fire_multiplier = 0.5
 
-    # FIRE is twice more likely to spread than COALS and SPARKS
-    fire_probs = [0.5 * likelyhood_of_fire_multiplier, 0.35 * likelyhood_of_fire_multiplier, 0.25 * likelyhood_of_fire_multiplier]
+    # FIRE is twice more likely to spread than COALS and  4x more likely than from SPARKS
+    fire_probs = [likelyhood_of_fire_multiplier, 0.5 * likelyhood_of_fire_multiplier, 0.25 * likelyhood_of_fire_multiplier]
     # if near WATER reduce fire_probs by water_reduction_rate
     if near_water:
         fire_probs =  [x * water_reduction_rate for x in fire_probs]
 
     
     if tree_type in TREE_TYPES:
-        for dx,dy in neighbourhood:
+        for i in range(len(neighbourhood)):
             # The diagonally-adjacent trees are further away, so
             # only catch fire with a reduced probability:
-            if abs(dx) == abs(dy) and np.random.random() < 0.573:
-                continue
-            if X[iy+dy,ix+dx] == FIRE and np.random.random() < fire_probs[0]:
+            # if abs(dx) == abs(dy) and np.random.random() < 0.573:
+            #     continue
+
+            # with wind inflence
+            dx, dy = neighbourhood[i][0], neighbourhood[i][1]
+            wind_power = wind_infulence[wind_direction][i]
+            local_probability_of_spread = np.random.random()
+            chance_of_fire = wind_power * local_probability_of_spread
+            treshold = 0.5
+
+            if X[iy+dy,ix+dx] == FIRE and chance_of_fire * fire_probs[0] > treshold:
                 X1[iy,ix] = SPARKS
                 break
-            elif X[iy+dy,ix+dx] == SPARKS and np.random.random() < fire_probs[1]:
+            elif X[iy+dy,ix+dx] == SPARKS and chance_of_fire * fire_probs[1] > treshold:
                 X1[iy,ix] = SPARKS
                 break
-            elif X[iy+dy,ix+dx] == COALS and np.random.random() < fire_probs[2]:
+            elif X[iy+dy,ix+dx] == COALS and chance_of_fire * fire_probs[2] > treshold:
                 X1[iy,ix] = SPARKS
                 break
             else:
@@ -81,13 +107,22 @@ def iterate(X):
         for iy in range(1,ny-1):
             if X[iy,ix] == EMPTY and np.random.random() <= p:
                 X1[iy,ix] = SAPLING
-            if X[iy,ix] == DEAD_TREE and np.random.random() <= p:
-                X1[iy,ix] = SAPLING
+            if X[iy,ix] == DEAD_TREE:
+                if np.random.random() <= p:
+                    X1[iy,ix] = SAPLING
+                else:
+                    X1[iy,ix] = DEAD_TREE
             # if the cell was a sapling convert it to the tree
             if X[iy,ix] == SAPLING:
-                X1[iy,ix] = TREE
+                if np.random.random() <= P_sapling_to_tree:
+                    X1[iy,ix] = TREE
+                else:
+                    X1[iy,ix] = SAPLING
             if X[iy,ix] == TREE:
-                X1[iy,ix] = OLD_TREE
+                if np.random.random() <= p_tree_to_old_tree:
+                    X1[iy,ix] = OLD_TREE
+                else:
+                    X1[iy,ix] = TREE
             if X[iy,ix] == OLD_TREE:
                 X1[iy,ix] = OLD_TREE
             if X[iy,ix] == OLD_TREE and np.random.random() <= p_tree_death:
@@ -110,6 +145,8 @@ def iterate(X):
 # Probability of new tree growth per empty cell, and of lightning strike.
 p, f = 0.05, 0.0001
 p_tree_death = 0.01
+p_tree_to_old_tree = 0.25
+P_sapling_to_tree = 0.35
 # Forest size (number of cells in x and y directions).
 nx, ny = 100, 100
 # Initialize the forest grid.
@@ -133,4 +170,5 @@ animate.X = X
 # Interval between frames (ms).
 interval = 100
 anim = animation.FuncAnimation(fig, animate, interval=interval, frames=200)
-plt.show()
+anim.save(f'animations/animation_water_influence_{water_reduction_rate}_wind_direction_{wind_direction}.gif', fps=60)
+#plt.show()
